15c99246116526fd84372d276071182d326035df
Rene Delgado <rene854@gmail.com>
2020-10-24 16:28:51 -0300

    Add chapter4

---
+++ chapter4/env.py
@@ -0,0 +1,146 @@
+from typing import Tuple, Type, List
+from functools import lru_cache
+from abc import ABCMeta, abstractmethod
+
+import numpy as np
+from scipy.stats import poisson
+
+
+class Space:
+    def __init__(self, dims: List[int], min: float, max: float, dtype: Type[int]):
+        self._dims = dims
+        self._min = dtype(min)
+        self._max = dtype(max)
+
+    @property
+    def dims(self):
+        return self._dims
+
+    @property
+    def min(self):
+        return self._min
+
+    @property
+    def max(self):
+        return self._max
+
+
+class Enviroment(metaclass=ABCMeta):
+    @abstractmethod
+    def dynamics(self, *args, **kwargs) -> float:
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def obs_space() -> Space:
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def act_space() -> Space:
+        raise NotImplementedError
+
+
+class RentalCarEnv(Enviroment):
+    # How many values of the Poisson distributtion use to calculate the dynamics
+    _poisson_range = 11
+    # Rental car lambdas
+    _lam_in_a = 3
+    _lam_out_a = 3
+    _lam_in_b = 4
+    _lam_out_b = 2
+    # Reward values
+    _move_reward = -2
+    _rental_reward = 10
+
+    def __init__(self, value: np.array, gamma=0.9, fixed_return=True):
+        self.gamma = gamma
+        self.fixed_return = fixed_return
+        self.value = value
+        # PMFs
+        self._return_a = poisson.pmf(range(self._poisson_range), self._lam_in_a)
+        self._request_a = poisson.pmf(range(self._poisson_range), self._lam_out_a)
+        self._return_b = poisson.pmf(range(self._poisson_range), self._lam_in_b)
+        self._request_b = poisson.pmf(range(self._poisson_range), self._lam_out_b)
+        # Instantiate spaces to reduce allocations
+        self._obs_space = self.obs_space()
+        self._act_space = self.act_space()
+
+    @staticmethod
+    def obs_space() -> Space:
+        return Space([2], 0, 20, int)
+
+    @staticmethod
+    def act_space() -> Space:
+        return Space([1], -5, 5, int)
+
+    @staticmethod
+    @lru_cache(maxsize=None)
+    def legal_actions(state: Tuple[int, int]) -> np.array:
+        return np.arange(
+            np.max([-state[1], RentalCarEnv.act_space().min]),
+            np.min([state[0], RentalCarEnv.act_space().max]) + 1,
+            1,
+            dtype=np.int32,
+        )
+
+    def dynamics(self, state: Tuple[int, int], action: int):
+        value = 0.0
+        reward_base = np.abs(action) * np.float32(self._move_reward)
+        state = list(state)
+        state[0] -= action
+        state[1] += action
+
+        for request_a in range(self._poisson_range):
+            request_a_prob = self._request_a[request_a]
+            for request_b in range(self._poisson_range):
+                request_b_prob = self._request_b[request_b]
+
+                real_request_a = np.min([request_a, state[0]])
+                real_request_b = np.min([request_b, state[1]])
+
+                reward = reward_base + np.float32(self._rental_reward) * (
+                    real_request_a + real_request_b
+                )
+
+                prob = request_a_prob * request_b_prob
+
+                if self.fixed_return:
+                    return_a = self._lam_in_a
+                    return_b = self._lam_in_b
+                    new_state = (
+                        np.min(
+                            [state[0] - real_request_a + return_a, self._obs_space.max]
+                        ),
+                        np.min(
+                            [state[1] - real_request_b + return_b, self._obs_space.max]
+                        ),
+                    )
+                    value += prob * (
+                        reward + self.gamma * self.value[new_state[0], new_state[1]]
+                    )
+                else:
+                    for return_a in range(self._poisson_range):
+                        return_a_prob = self._return_a[return_a]
+                        for return_b in range(self._poisson_range):
+                            return_b_prob = self._return_b[return_b]
+                            _prob = prob * return_a_prob * return_b_prob
+                            new_state = (
+                                np.min(
+                                    [
+                                        state[0] - real_request_a + return_a,
+                                        self._obs_space.max,
+                                    ]
+                                ),
+                                np.min(
+                                    [
+                                        state[1] - real_request_b + return_b,
+                                        self._obs_space.max,
+                                    ]
+                                ),
+                            )
+                            value += _prob * (
+                                reward
+                                + self.gamma * self.value[new_state[0], new_state[1]]
+                            )
+        return value
---
+++ chapter4/learner.py
@@ -0,0 +1,103 @@
+from functools import lru_cache
+from typing import Type
+
+import numpy as np
+
+from env import Enviroment
+
+
+class Learner:
+    def __init__(self, env: Type[Enviroment], eps: float = 0.001, **env_args):
+        self.obs_space_range = env.obs_space().max - env.obs_space().min + 1
+        self.action_space_range = env.act_space().max - env.act_space().min + 1
+        self.action_probability = 1.0 / self.action_space_range
+        self.obs_space_shape = [
+            self.obs_space_range for _ in range(env.obs_space().dims[0])
+        ]
+        self.eps = eps
+        self.reset()
+        self.env = env(self.value, **env_args)
+
+    def reset(self):
+        self.value = np.zeros(shape=self.obs_space_shape)
+        self.policy = np.zeros(shape=self.obs_space_shape, dtype=np.int32)
+
+    def policy_improvement(self):
+        print("Running policy improvement")
+        stable = True
+        with np.nditer(
+            [self.policy], flags=["multi_index"], op_flags=[["readwrite"]]
+        ) as it:
+            while not it.finished:
+                old_action = np.int32(it[0])
+                actions = self.env.legal_actions(it.multi_index)
+                q = np.zeros_like(actions, dtype=np.float32)
+                for a, action in enumerate(actions):
+                    q[a] += self.env.dynamics(it.multi_index, action)
+                it[0][...] = actions[np.argmax(q)]
+                if np.int32(it[0]) != old_action:
+                    stable = False
+                it.iternext()
+        return stable
+
+    def policy_evaluation(self):
+        print("Running policy evaluation")
+        converged = False
+        while not converged:
+            old_value = self.value.copy()
+            with np.nditer(
+                [self.value, self.policy],
+                flags=["multi_index"],
+                op_flags=[["readwrite"], ["readonly"]],
+            ) as it:
+                while not it.finished:
+                    it[0][...] = self.env.dynamics(it.multi_index, it[1])
+                    it.iternext()
+            max_diff = np.max(np.abs(self.value - old_value))
+            print(f"Max diff: {max_diff}")
+            if max_diff < self.eps:
+                converged = True
+                print("Converged")
+
+    # def plot_policy(self, ax=None, figsize=(6, 6)):
+    #     if ax is None:
+    #         fig, ax = plt.subplots(figsize=figsize)
+    #     img = np.flipud(self.policy)
+    #     im = ax.imshow(img)
+    #     # We want to show all ticks...
+    #     ax.set_xticks(np.arange(self.obs_space_range))
+    #     ax.set_yticks(np.flip(np.arange(self.obs_space_range)))
+
+    #     ax.set_xticklabels(np.arange(self.obs_space_range))
+    #     ax.set_yticklabels(np.arange(self.obs_space_range))
+
+    #     # Rotate the tick labels and set their alignment.
+    #     plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+    #     # Loop over data dimensions and create text annotations.
+    #     for i in range(self.obs_space_range):
+    #         for j in range(self.obs_space_range):
+    #             text = ax.text(j, i, img[i, j], ha="center", va="center", color="w")
+
+    #     ax.set_title("Policy")
+
+    # def plot_value(self, ax=None, figsize=(6, 6)):
+    #     if ax is None:
+    #         fig, ax = plt.subplots(figsize=figsize)
+    #         ax = fig.add_subplot(111, projection="3d")
+
+    #     x = np.arange(self.obs_space_range)
+    #     y = np.arange(self.obs_space_range)
+    #     X, Y = np.meshgrid(x, y)
+    #     ax.plot_surface(X, Y, self.value, cmap=cm.coolwarm)
+    #     ax.set_xticks(np.arange(self.obs_space_range))
+    #     ax.set_yticks(np.flip(np.arange(self.obs_space_range)))
+
+    #     ax.set_xticklabels(np.arange(self.obs_space_range))
+    #     ax.set_yticklabels(np.arange(self.obs_space_range))
+    #     ax.set_title("Value")
+
+    # def plot(self, figsize=(12, 6)):
+    #     fig = plt.figure(figsize=figsize)
+    #     self.plot_policy(fig.add_subplot(121))
+    #     self.plot_value(fig.add_subplot(122, projection="3d"))
