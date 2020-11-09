54205160aa3de406c2bb0c3149a8ede831ccd459
Rene Delgado <rene854@gmail.com>
2020-11-08 22:57:30 -0300

    Add Trajectory and Step classes

---
+++ chapter5/trajectory.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+
+@dataclass
+class Step:
+    is_final: bool
+    state: Tuple[int, ...]
+    action: int
+    reward: float
+
+
+class Trajectory:
+    def __init__(self):
+        self.steps = []
+
+    def add_step(
+        self, is_final: bool, state: Tuple[int, ...], action: int, reward: float
+    ):
+        self.steps.append(Step(is_final, state, action, reward))
+
+    def __len__(self):
+        return len(self.steps)
+
+    def __getitem__(self, index: int):
+        return self.steps[index]
+
+    def __repr__(self):
+        return "\n".join(
+            [f"t={i}: {step.__dict__}" for i, step in enumerate(self.steps)]
+        )
