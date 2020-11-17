7cabfa653b8f55026de249f8a32c048c8e7e872c
Rene Delgado <rene854@gmail.com>
2020-11-16 22:26:15 -0300

    improve figure 5_3

--- chapter5/main.py
+++ chapter5/main.py
@@ -96,81 +96,43 @@ def figure_5_2(figsize=(12, 12)):
 
 
 def figure_5_3(figsize=(12, 12)):
+
+    true_value = -0.27726
     init_state = (13, 1, 2)
+    idx_state = (1, 1, 1)
+
+    target_policy = np.ones((10, 2, 10), dtype=np.int32)
+    target_policy[-2:, :, :] = 0
 
     env = BlackJack()
-    policy = lambda state: state[0] < 20
-    mc = MonteCarloPredictor(env)
-    mc.predict_on_policy(policy, n_iters=1_000_000, init_state=init_state)
+    iters_arr = [10 ** x for x in range(6)]
+    runs = 100
 
-    print(mc.V[1, 1, 1])
+    run_Vs = []
+    for _ in tqdm(range(runs)):
+        iter_Vs = []
+        mc_off = MonteCarloController(env)
+        for iterations in tqdm([iters_arr[0]] + np.diff(iters_arr).tolist()):
+            mc_off.off_policy_predict(
+                target_policy,
+                epsilon=0.3,
+                iters=iterations,
+                init_state=init_state,
+                disable_tqdm=True,
+            )
+            iter_Vs.append(mc_off.Q[idx_state].max(-1))
+        run_Vs.append(iter_Vs)
+
+    err = np.power(true_value - np.array(run_Vs).mean(0), 2)
 
     fig = plt.figure(figsize=figsize)
-
-    # fig.savefig("figure_5_1.png", dpi=100)
-
-    # true_value = -0.27726
-    # init_state = (13, 1, 2)
-    # idx_state = (1, 1, 1)
-
-    # target_policy = np.ones((10, 2, 11), dtype=np.int32)
-    # target_policy[-2:, :, :] = 0
-
-    # env = BlackJack()
-    # n_iterations = [10 ** x for x in range(6)]
-    # n_iterations = [10_000]
-    # runs = 10
-
-    # iter_values = np.zeros(len(n_iterations))
-    # for i, iterations in tqdm(enumerate(n_iterations)):
-    #     runs_values = np.zeros(runs, dtype=np.float32)
-    #     for run in tqdm(range(runs)):
-    #         mc_off = MonteCarloController(env)
-    #         mc_off.off_policy_predict(
-    #             target_policy,
-    #             iters=iterations,
-    #             init_state=init_state,
-    #             disable_tqdm=False,
-    #         )
-    #         runs_values[run] = mc_off.state_action_value[idx_state].mean(-1)
-    #         print(runs_values[: run + 1].mean())
-    #     iter_values[i] = np.power(runs_values.mean() - true_value, 2)
-
-    # fig = plt.figure(figsize=figsize)
-    # ax = fig.add_subplot(111)
-    # ax.plot(n_iterations, iter_values)
-    # ax.set_xscale("log")
-    # fig.savefig("figure_5_3.png", dpi=100)
-
-    # fig = plt.figure(figsize=figsize)
-    # ax = fig.add_subplot(222, projection="3d")
-    # plot_state_value(
-    #     mc_off.state_value[:, 1, :], ax=ax, title=f"Value usable ace {iterations} iters"
-    # )
-    # ax = fig.add_subplot(224, projection="3d")
-    # plot_state_value(
-    #     mc_off.state_value[:, 0, :],
-    #     ax=ax,
-    #     title=f"value no usable ace {iterations} iters",
-    # )
-
-    # ax = fig.add_subplot(221)
-    # plot_policy(
-    #     mc_off.greedy_policy[:, 1, :],
-    #     ax=ax,
-    #     title=f"Policy usable ace {iterations} iters",
-    # )
-    # ax = fig.add_subplot(223)
-    # plot_policy(
-    #     mc_off.greedy_policy[:, 0, :],
-    #     ax=ax,
-    #     title=f"Policy no usable ace {iterations} iters",
-    # )
-
-    # fig.savefig("figure_5_3.png", dpi=100)
+    ax = fig.add_subplot(111)
+    ax.plot(iters_arr, err)
+    ax.set_xscale("log")
+    fig.savefig("figure_5_3.png", dpi=100)
 
 
 if __name__ == "__main__":
-    figure_5_1()
-    figure_5_2()
-    # figure_5_3()
+    # figure_5_1()
+    # figure_5_2()
+    figure_5_3()
