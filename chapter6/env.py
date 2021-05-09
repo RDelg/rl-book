206667e34e1f13201f046632f68f501c9af3b9e9
Rene Delgado <rene854@gmail.com>
2021-05-09 14:33:30 -0400

    update randomwalk env

--- chapter6/env.py
+++ chapter6/env.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from chapter5.env import Enviroment, Space
+from chapter5.env import Enviroment, DiscreteDim, DiscreteSpace
 from chapter5.types import Observation, State
 
 
@@ -8,15 +8,15 @@ class RandomWalk(Enviroment):
     def __init__(self, n: int = 5):
         assert n % 2, "n must be odd"
         self.n = n
-        self._obs_space = Space(dims=1, min=0, max=self.n)
-        self._act_space = Space(dims=1, min=0, max=1)
+        self._act_space = DiscreteDim(1, minimum=0)
+        self._obs_space = DiscreteSpace(DiscreteDim(self.n))
         self.reset()
 
     def reset(self):
         self.state = (self.n // 2) + 1
 
     def step(self, action: int) -> Observation:
-        assert 0 <= action <= 1
+        assert self.act_space.contains(action), "Invalid action"
         self.state += action
         if self.state > self.n:
             done = True
@@ -28,6 +28,3 @@ class RandomWalk(Enviroment):
             done = False
             reward = 0
         return Observation(done, self.state, reward)
-
-    def legal_actions(self, state: State) -> np.ndarray:
-        return np.array([0, 1], dtype=np.int32)
