40b94473f5008b4f3db912afd4ac02e45d76990b
Rene Delgado <RDelg@users.noreply.github.com>
2022-03-20 19:03:58 -0300

    Update chapter 2 (#5)
    * fig 2.1 and 2.2 updated
    
    * create factories
    
    * add figure 2.3
    
    * figs until 2.5
    
    * add image 2.6
    
    * update images

--- chapter2/learner.py
+++ chapter2/learner.py
@@ -1,135 +1,171 @@
-from typing import Union, List
+from abc import ABCMeta, abstractmethod
+from dataclasses import dataclass
+from typing import Callable, Tuple
 
 import numpy as np
 
-from .env import Enviroment
+from .env import KArmedEnviroment
 
 
-class KBanditLearner:
+@dataclass
+class Observation:
     """
-    K-arms bandit learner.
+    Observation class for the KBandit enviroment.
+    """
+
+    action: int
+    reward: np.ndarray
+
+
+class BaseLearner(metaclass=ABCMeta):
+    """
+    Base learner class.
+    """
+
+    def __init__(self, env: KArmedEnviroment):
+        self.env = env
+        self.reset()
+
+    def reset(self):
+        self.Q = np.zeros(self.env.k)
+        self.env.reset()
+
+    @abstractmethod
+    def play_one(self, policy: Callable[[np.ndarray], int]) -> Observation:
+        return
+
+
+class AvgLearner(BaseLearner):
+    """
+    Learner that uses the mean of the rewards as the estimate of the reward.
+    """
+
+    def __init__(self, env: KArmedEnviroment, **kwargs):
+        super().__init__(env)
+
+    def play_one(self, policy: Callable[[np.ndarray], int]) -> Observation:
+        action = policy(self.Q)
+        reward = self.env.step(action)
+        self.actions_count[action] += 1
+
+        self.Q[action] += 1 / self.actions_count[action] * (reward - self.Q[action])
+        self.steps += 1
+        return Observation(action=action, reward=reward)
+
+    def reset(self):
+        self.actions_count = np.zeros(self.env.k)
+        self.steps = 0
+        super().reset()
+
+
+class UCBLearner(BaseLearner):
+    """
+    Upper Confidence Bound (UCB) algorithm.
 
     Parameters
     ----------
-    env : Enviroment
-        Instanciated enviroment that the learner will use.
-    
-    k : int
-        Number of "arms" in the bandit enviroment.
-
-    eps : float, default=0.01
-        Epsilon variable in eps-greedy algorithm.
-
-    delta : float, default=0.01
-        Step size to use in grandient base updates and constant value updates.
-
-    initial : float, list of floats, default=0.01
-        Constant or list of contants where the stimated reward starts at time 0.
-
     c : float, default=0.0
         Uncertainty variable used in UCB algorithm.
 
-    eps : float, default=0.01
-        Epsilon variable in eps-greedy algorithm.
-
-    gradient : bool, default=False
-        If True, use gradient based bandit algorithm
-
-    gradient_base : bool, default=True
-        If True, use average reward as baseline for gradient based bandit algorithm.
-    
+    initial_Q : float, default=0.0
+        Initial value for the Q-table.
     """
 
     def __init__(
-        self,
-        env: Enviroment,
-        k: int,
-        eps: float = 0.01,
-        delta: float = None,
-        initial: Union[float, List[float]] = 0.0,
-        c: float = 0.0,
-        gradient: bool = False,
-        gradient_base: bool = True,
+        self, env: KArmedEnviroment, initial_Q: float = 0.0, c: float = 0.0, **kwargs
     ):
-        assert issubclass(type(env), Enviroment), "env must be an Enviroment subclass"
-        self.env = env
-        self.k = k
-        self.eps = eps
-        self.delta = delta
-        self.estimated_reward = np.zeros(self.k) + initial
-        self.actions_count = np.zeros(self.k)
+        self.initial_Q = initial_Q
         self.c = c
-        self.gradient = gradient
-        self.gradient_base = gradient_base
+        super().__init__(env)
 
-    def play(self, step_size):
-        # Inicialize
-        self.actions_taken = np.zeros(step_size)
-        self.obtained_reward = np.zeros(step_size)
-        baseline = 0
-        one_hot = np.zeros(self.k)
-        # Run env
-        reward = self.env.reward(step_size)
-        # Play
-        random_choise = np.random.choice(a=np.arange(self.k), size=(step_size))
-        is_greedy = np.random.uniform(size=(step_size)) > self.eps
-
-        for i in range(step_size):
-            # Taking the action
-            if self.gradient:
-                exp_est = np.exp(self.estimated_reward)
-                self.action_prob = exp_est / np.sum(exp_est)
-                action = (np.cumsum(self.action_prob) >= np.random.uniform()).argmax(
-                    axis=0
-                )
-            elif self.c > 0:
-                action = (
-                    np.argmax(
-                        self.estimated_reward
-                        + self.c
-                        * np.sqrt(np.divide(np.log(i + 1), self.actions_count + 1e-5))
-                    )
-                    if is_greedy[i]
-                    else random_choise[i]
-                )
-            else:
-                action = (
-                    np.argmax(self.estimated_reward)
-                    if is_greedy[i]
-                    else random_choise[i]
-                )
-            self.actions_count[action] += 1
-            self.actions_taken[i] = action
-
-            # Update estimation
-            if self.gradient:
-                one_hot[:] = 0
-                one_hot[action] = 1
-                if self.gradient_base:
-                    baseline += (reward[i, action] - baseline) / (i + 1)
-                else:
-                    baseline = 0
-                self.estimated_reward += (
-                    self.delta
-                    * (reward[i, action] - baseline)
-                    * (one_hot - self.action_prob)
-                )
-            elif self.delta is None:
-                self.estimated_reward[action] += (
-                    1
-                    / self.actions_count[action]
-                    * (reward[i, action] - self.estimated_reward[action])
-                )
-            else:
-                self.estimated_reward[action] += self.delta * (
-                    reward[i, action] - self.estimated_reward[action]
-                )
-
-            # Saving rewards
-            self.obtained_reward[i] = reward[i, action]
+    def play_one(self, policy: Callable[[np.ndarray], int]) -> Observation:
+        action = policy(
+            self.Q
+            + self.c
+            * np.sqrt(np.divide(np.log(self.steps + 1), self.actions_count + 1e-5))
+        )
+        reward = self.env.step(action)
+        self.actions_count[action] += 1
+        self.Q[action] += 1 / self.actions_count[action] * (reward - self.Q[action])
+        self.steps += 1
+        return Observation(action=action, reward=reward)
 
     def reset(self):
-        self.env.reset()
-        self.estimated_reward = np.zeros(self.k)
-        self.actions_count = np.zeros(self.k)
+        self.actions_count = np.zeros(self.env.k)
+        self.steps = 0
+        super().reset()
+        self.Q += self.initial_Q
 
+
+class GradientLearner(BaseLearner):
+    """
+    Gradient base learner
+    """
+
+    def __init__(
+        self, env: KArmedEnviroment, delta: float, gradient_base: bool = True, **kwargs
+    ):
+        self.delta = delta
+        self.gradient_base = gradient_base
+        super().__init__(env)
+
+    def play_one(
+        self, policy: Callable[[np.ndarray], Tuple[int, np.ndarray]]
+    ) -> Observation:
+        action, self.probs = policy(self.Q)
+        reward = self.env.step(action)
+        self.steps += 1
+
+        self.one_hot[:] = 0.0
+        self.one_hot[action] = 1.0
+        if self.gradient_base:
+            self.baseline += (reward - self.baseline) / (self.steps)
+        self.Q += self.delta * (reward - self.baseline) * (self.one_hot - self.probs)
+        return Observation(action=action, reward=reward)
+
+    def reset(self):
+        self.one_hot = np.zeros(self.env.k)
+        self.probs = np.ones(self.env.k) / self.env.k
+        self.steps = 0
+        self.baseline = 0.0
+        super().reset()
+
+
+def random_policy(Q: np.ndarray) -> int:
+    """
+    Random policy.
+    """
+    return np.random.randint(0, Q.size)
+
+
+def e_greedy_policy(epsilon: float) -> Callable[[np.ndarray], int]:
+    """
+    Epsilon-greedy policy.
+    """
+    eps = epsilon
+
+    def policy(Q: np.ndarray) -> int:
+        if np.random.random() < eps:
+            return np.random.randint(0, Q.size)
+        else:
+            return np.argmax(Q)
+
+    return policy
+
+
+def greedy_policy(Q: np.ndarray) -> int:
+    """
+    Greedy policy.
+    """
+    return np.argmax(Q)
+
+
+def gradient_policy(Q: np.ndarray) -> Tuple[int, np.ndarray]:
+    """
+    Gradient policy.
+    """
+
+    exp_est = np.exp(Q)
+    action_prob = exp_est / np.sum(exp_est)
+    action = (np.cumsum(action_prob) >= np.random.uniform()).argmax(axis=0)
+    return action, action_prob
--- chapter2/main.py
+++ chapter2/main.py
@@ -4,13 +4,21 @@ import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-from chapter2.env import KBandit
-from chapter2.learner import KBanditLearner
+from chapter2.env import NormalKBandit
+from chapter2.learner import (
+    AvgLearner,
+    UCBLearner,
+    GradientLearner,
+    gradient_policy,
+    e_greedy_policy,
+    greedy_policy,
+)
 
 
 class Experiment:
     """
     Helper class to run K-Armed Bandit experiments.
+
 
     Parameters
     ----------
@@ -39,17 +47,47 @@ class Experiment:
         self.hist_best_action = np.zeros(shape=(self.runs, self.n_configs))
         self.pct_correct = None
 
+    def env_factory(self, config: dict):
+        return NormalKBandit(k=self.k, **config.get("env", {}))
+
+    def learner_factory(self, env, config: dict):
+        T = config.get("learner", {}).get("type", "avg")
+        if T == "avg":
+            return AvgLearner(env=env, **config.get("learner", {}).get("params", {}))
+        elif T == "ucb":
+            return UCBLearner(env=env, **config.get("learner", {}).get("params", {}))
+        elif T == "gradient":
+            return GradientLearner(
+                env=env, **config.get("learner", {}).get("params", {})
+            )
+        else:
+            raise ValueError(f"Learner {T} not implemented")
+
+    def policy_factory(self, config: dict):
+        T = config.get("policy", {}).get("type", "e_greedy")
+        if T == "e_greedy":
+            return e_greedy_policy(**config.get("policy", {}).get("params", {}))
+        elif T == "greedy":
+            return greedy_policy
+        elif T == "gradient":
+            return gradient_policy
+        else:
+            raise ValueError(f"Policy {T} not implemented")
+
     def run(self):
         for i, c in enumerate(tqdm(self.configs)):
-            for n in tqdm(range(self.runs)):
-                env_config = c.get("env", {})
-                learner_config = c["learner"]
-                env = KBandit(k=self.k, **env_config)
-                learner = KBanditLearner(env, self.k, **learner_config)
-                learner.play(self.t)
-                self.hist_R[:, n, i] = learner.obtained_reward
-                self.hist_A[:, n, i] = learner.actions_taken
+            for n in tqdm(range(self.runs), leave=False):
+                env = self.env_factory(c)
+                learner = self.learner_factory(env, c)
+                policy = self.policy_factory(c)
+                for t in range(self.t):
+                    obs = learner.play_one(policy)
+                    self.hist_R[t, n, i] = obs.reward
+                    self.hist_A[t, n, i] = obs.action
                 self.hist_best_action[n, i] = env.best_action
+
+                # print("learner", learner.Q)
+                # print("env", env.mean)
 
     def pct_correct_over_time(self):
         self.pct_correct = np.ones(shape=(self.t, len(self.configs)))
@@ -77,15 +115,19 @@ def figure_2_1():
     print(f"Running figure_2_1")
     k = 10
     t = 10000
-    env = KBandit(k=k)
-    reward = env.reward(t)
+    env = NormalKBandit(k=k)
+    rewards = []
+    for action in range(k):
+        reward = [env.step(action) for _ in range(t)]
+        rewards.append(reward)
+
     # plot
     f = plt.figure(figsize=(12, 8))
     ax = f.subplots(1, 1)
-    ax.violinplot(reward)
+    ax.violinplot(rewards)
     ax.set_xlabel("Action", size=20)
     ax.set_ylabel("Reward\n distribution", size=20)
-    f.savefig("figure_2_1.png", dpi=200)
+    f.savefig("figure_2_1.png", dpi=100)
 
 
 def figure_2_2():
@@ -94,9 +136,9 @@ def figure_2_2():
     t = 1000
     runs = 2000
     configs = [
-        {"learner": {"eps": 0.0}},
-        {"learner": {"eps": 0.1}},
-        {"learner": {"eps": 0.01}},
+        {"policy": {"type": "e_greedy", "params": {"epsilon": 0.0}}},
+        {"policy": {"type": "e_greedy", "params": {"epsilon": 0.1}}},
+        {"policy": {"type": "e_greedy", "params": {"epsilon": 0.01}}},
     ]
 
     exp = Experiment(k, t, runs, configs)
@@ -107,7 +149,7 @@ def figure_2_2():
     axs = f.subplots(2, 1)
     exp.plot_mean_reward(axs[0])
     exp.plot_pct_correct(axs[1])
-    f.savefig("figure_2_2.png", dpi=200)
+    f.savefig("figure_2_2.png", dpi=100)
 
 
 def figure_2_3():
@@ -117,8 +159,11 @@ def figure_2_3():
     runs = 2000
 
     configs = [
-        {"learner": {"eps": 0.0, "initial": 5}},
-        {"learner": {"eps": 0.1}},
+        {
+            "policy": {"type": "greedy"},
+            "learner": {"type": "ucb", "params": {"initial_Q": 5}},
+        },
+        {"policy": {"type": "e_greedy", "params": {"epsilon": 0.1}}},
     ]
 
     exp = Experiment(k, t, runs, configs)
@@ -129,7 +174,7 @@ def figure_2_3():
     axs = f.subplots(2, 1)
     exp.plot_mean_reward(axs[0])
     exp.plot_pct_correct(axs[1])
-    f.savefig("figure_2_3.png", dpi=200)
+    f.savefig("figure_2_3.png", dpi=100)
 
 
 def figure_2_4():
@@ -138,7 +183,13 @@ def figure_2_4():
     t = 1000
     runs = 1000
 
-    configs = [{"learner": {"eps": 0.0, "c": 2}}, {"learner": {"eps": 0.1}}]
+    configs = [
+        {
+            "learner": {"type": "ucb", "params": {"c": 2, "initial_Q": 0.0}},
+            "policy": {"type": "greedy"},
+        },
+        {"policy": {"type": "e_greedy", "params": {"epsilon": 0.1}}},
+    ]
 
     exp = Experiment(k, t, runs, configs)
     exp.run()
@@ -148,7 +199,7 @@ def figure_2_4():
     axs = f.subplots(2, 1)
     exp.plot_mean_reward(axs[0])
     exp.plot_pct_correct(axs[1])
-    f.savefig("figure_2_4.png", dpi=200)
+    f.savefig("figure_2_4.png", dpi=100)
 
 
 def figure_2_5():
@@ -157,21 +208,41 @@ def figure_2_5():
     t = 1000
     runs = 2000
 
+    k = 4
+    t = 500
+    runs = 500
+
     configs = [
         {
-            "learner": {"delta": 0.4, "gradient": True, "gradient_base": True},
+            "learner": {
+                "type": "gradient",
+                "params": {"delta": 0.4, "gradient_base": True},
+            },
+            "policy": {"type": "gradient"},
             "env": {"offset": 4},
         },
         {
-            "learner": {"delta": 0.1, "gradient": True, "gradient_base": True},
+            "learner": {
+                "type": "gradient",
+                "params": {"delta": 0.1, "gradient_base": True},
+            },
+            "policy": {"type": "gradient"},
             "env": {"offset": 4},
         },
         {
-            "learner": {"delta": 0.4, "gradient": True, "gradient_base": False},
+            "learner": {
+                "type": "gradient",
+                "params": {"delta": 0.4, "gradient_base": False},
+            },
+            "policy": {"type": "gradient"},
             "env": {"offset": 4},
         },
         {
-            "learner": {"delta": 0.1, "gradient": True, "gradient_base": False},
+            "learner": {
+                "type": "gradient",
+                "params": {"delta": 0.1, "gradient_base": False},
+            },
+            "policy": {"type": "gradient"},
             "env": {"offset": 4},
         },
     ]
@@ -184,7 +255,7 @@ def figure_2_5():
     axs = f.subplots(2, 1)
     exp.plot_mean_reward(axs[0])
     exp.plot_pct_correct(axs[1])
-    f.savefig("figure_2_5.png", dpi=200)
+    f.savefig("figure_2_5.png", dpi=100)
 
 
 def figure_2_6():
@@ -199,13 +270,33 @@ def figure_2_6():
     c_UCB = np.logspace(-4, 2, num=n, base=2)
     q_optimistic = np.logspace(-2, 3, num=n, base=2)
 
-    cfg_eps_greedy = [{"learner": {"eps": x}} for x in eps_greedy]
-    cfg_delta_gradient = [
-        {"learner": {"delta": x, "gradient": True}} for x in delta_gradient
+    cfg_eps_greedy = [
+        {
+            "learner": {"type": "avg"},
+            "policy": {"type": "e_greedy", "params": {"epsilon": x}},
+        }
+        for x in eps_greedy
     ]
-    cfg_c_UCB = [{"learner": {"c": x}} for x in c_UCB]
+    cfg_delta_gradient = [
+        {
+            "learner": {"type": "gradient", "params": {"delta": x}},
+            "policy": {"type": "gradient"},
+        }
+        for x in delta_gradient
+    ]
+    cfg_c_UCB = [
+        {
+            "learner": {"type": "ucb", "params": {"c": x}},
+            "policy": {"type": "e_greedy", "params": {"epsilon": 0.01}},
+        }
+        for x in c_UCB
+    ]
     cfg_q_optimistic = [
-        {"learner": {"eps": 0, "delta": 0.1, "initial": x}} for x in q_optimistic
+        {
+            "learner": {"type": "ucb", "params": {"delta": 0.1, "initial_Q": x}},
+            "policy": {"type": "greedy"},
+        }
+        for x in q_optimistic
     ]
 
     xs = [eps_greedy, delta_gradient, c_UCB, q_optimistic]
@@ -221,17 +312,17 @@ def figure_2_6():
     f = plt.figure(figsize=(16, 8))
     ax = f.subplots(1, 1)
     for x, y in zip(xs, ys):
-        ax.semilogx(x, y, basex=2)
+        ax.semilogx(x, y, base=2)
     ax.legend(["eps-greedy", "delta_gradient", "c_UCB", "q_optimistic delta=0.1"])
     ax.set_xlabel("Action", size=20)
     ax.set_ylabel("Average\n reward\n over first\n 1000 steps", size=20)
-    f.savefig("figure_2_6.png", dpi=200)
+    f.savefig("figure_2_6.png", dpi=100)
 
 
 if __name__ == "__main__":
-    figure_2_1()
-    figure_2_2()
-    figure_2_3()
-    figure_2_4()
-    figure_2_5()
+    # figure_2_1()
+    # figure_2_2()
+    # figure_2_3()
+    # figure_2_4()
+    # figure_2_5()
     figure_2_6()
